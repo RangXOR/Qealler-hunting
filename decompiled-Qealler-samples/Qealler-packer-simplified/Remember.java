eb300fe6cde3dc285f63db6aaf27f0285b5812d5
Abjuri5t <rangxor@protonmail.com>
2021-03-09 23:29:14 -0500

    simplified version of Qealler

---
+++ decompiled-Qealler-samples/Qealler-packer-simplified/Remember.java
@@ -0,0 +1,11 @@
+import java.io.DataInputStream;
+
+public class Remember {
+	public static Class selfRef;
+	public static DataInputStream rsrcData;
+	public static String undistinct;public static String approvable;
+	
+	static {
+		Remember.selfRef = Remember.class;
+	}
+}
